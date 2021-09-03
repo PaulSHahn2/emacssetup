24bc2e5728e88e907e71df35c8a03455923f4b71
Paul S Hahn <685483+PaulSHahn@users.noreply.github.com>
2021-09-03 01:31:12 -0700

    Modified to assign applications to workspaces by category.
    This replaces assigning apps to workspaces via hardcoded appname.

--- custom/setup-exwm.el
+++ custom/setup-exwm.el
@@ -5,9 +5,10 @@
 
 ;; this should be a built-in as long as you compiled with dbus mode
 (require 'notifications)
+;; builtin for free desktop support
 (require 'xdg)
 
-;; possible free desktop locations for desktop files
+;; possible free desktop locations for desktop configuration files
 (defvar-local exwmsetup/xdg-app-directories
   (mapcar (lambda (dir) (expand-file-name "applications" dir))
           (xdg-data-dirs)))
@@ -15,37 +16,40 @@
 ;; hash table with xdg application data stored by window class name
 (defvar-local exwmsetup/exwm-xdg-program-categories-hash (make-hash-table :test 'equal :size 500))
 
-(defun exwmsetup/goto-previous-frame ()
+;; hash table holds app category to workspace index affiliation
+(defvar-local exwmsetup/xdg-category-workspace-hash (make-hash-table :test 'equal :size 15))
+
+(defun exwmsetup/goto-previous-workspace ()
   "Go to the frame one less than current by index, or wrap to 0 if last frame."
   (interactive)
-  (defvar-local my-what-workspace 0)
+  (defvar-local moveto-workspace 0)
   (if (= exwm-workspace-current-index 0)
-       (setq my-what-workspace (- exwm-workspace-number 1))
-    (setq my-what-workspace (- exwm-workspace-current-index 1))
+       (setq moveto-workspace (- exwm-workspace-number 1))
+    (setq moveto-workspace (- exwm-workspace-current-index 1))
    )
-  (exwm-workspace-switch-create my-what-workspace)
-  (notifications-notify :title "workspace" :body (number-to-string exwm-workspace-current-index) :timeout 1000)
+  (exwm-workspace-switch-create moveto-workspace)
+  (notifications-notify :title (format "workspace %d" exwm-workspace-current-index) :timeout 1000)
 )
 
-(defun exwmsetup/goto-next-frame ()
+(defun exwmsetup/goto-next-workspace ()
   "Go to the next frame by index, or wrap to 0 if last frame."
   (interactive)
-  (defvar-local my-what-workspace 0)
+  (defvar-local moveto-workspace 0)
   (if (= exwm-workspace-current-index (- exwm-workspace-number 1))
-      (setq my-what-workspace 0)
-    (setq my-what-workspace (+ exwm-workspace-current-index 1))
+      (setq moveto-workspace 0)
+    (setq moveto-workspace (+ exwm-workspace-current-index 1))
   )
-  (exwm-workspace-switch-create my-what-workspace)
-  (notifications-notify :title "workspace" :body (number-to-string exwm-workspace-current-index) :timeout 1000)
+  (exwm-workspace-switch-create moveto-workspace)
+  (notifications-notify :title (format "workspace %d" exwm-workspace-current-index) :timeout 1000)
 )
 
 (defun exwmsetup/get-xdg-desktop-file-data (desktop-file)
-  "For a given DESKTOP-FILE, get the xdg data in the file."
+  "For a given DESKTOP-FILE, get the xdg data in the file and add to exwmsetup/exwm-xdg-program-categories-hash with a key of classname or executable name."
   ;;(message "Found xdg desktop application file: %s" desktop-file)
-  (defvar-local local-temp-hash (xdg-desktop-read-file desktop-file))
-  (defvar-local local-class-name (elt (split-string (gethash "StartupWMClass" local-temp-hash (gethash "Exec" local-temp-hash "!KEY-MISSING")) " " 1) 0))
+  (setq local-temp-hash (xdg-desktop-read-file desktop-file))
+  (setq local-class-name (elt (split-string (gethash "StartupWMClass" local-temp-hash (gethash "Exec" local-temp-hash "!KEY-MISSING")) " " 1) 0))
   (unless (string-equal local-class-name  "!KEY-MISSING")
-    (puthash local-class-name local-temp-hash exwmsetup/exwm-xdg-program-categories-hash)))
+    (puthash (elt (last (split-string local-class-name "/" 1)) 0) local-temp-hash exwmsetup/exwm-xdg-program-categories-hash)))
 
 (defun exwmsetup/exwm-xdg-app-mapper ()
   "Get a hash table with xdg data for all installed x-windows applications.
@@ -56,9 +60,42 @@ The key is the exwm-class-name and the value is hash table of xdg data."
         (mapcan (lambda (current-dir) (directory-files current-dir 1 "\.desktop$"))
                 (seq-filter 'file-directory-p exwmsetup/xdg-app-directories))))
 
-(defun exwmsetup/get-xdg-desktop-category-list (exwm-class-name)
-  "Get the list of xdg categories the given application specified by EXWM-CLASS-NAME belongs too."
-  (split-string (gethash "Categories" (gethash exwm-class-name exwmsetup/exwm-xdg-program-categories-hash) "Unknown;") ";" 1))
+(defun exwmsetup/get-xdg-desktop-category-list (window-class-name)
+  "Get the list of xdg categories the given application specified by WINDOW-CLASS-NAME belongs too."
+  (if (hash-table-p (gethash window-class-name exwmsetup/exwm-xdg-program-categories-hash nil) )
+      (split-string (gethash "Categories" (gethash window-class-name exwmsetup/exwm-xdg-program-categories-hash) "Unknown;") ";" 1)
+  '("Unknown")))
+
+(defun exwmsetup/setup-category-workspace-affiliation ()
+  "Populate the xdg-category-workspace-hash table with a key of xdg category and a value of the mapped workspace to send the app to by index."
+  (puthash "Development" 1 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Network" 2 exwmsetup/xdg-category-workspace-hash)
+  (puthash "AudioVideo" 3 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Audio" 3 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Video" 3 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Graphics" 3 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Game" 3 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Office" 4 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Education" 4 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Science" 4 exwmsetup/xdg-category-workspace-hash)
+  (puthash "System" 5 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Settings" 5 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Utility" 5 exwmsetup/xdg-category-workspace-hash)
+  (puthash "Unknown" 5 exwmsetup/xdg-category-workspace-hash)
+  )
+
+(defun exwmsetup/configure-window-by-app-category ()
+  "When an x windows program is started, compare each of its categories to the workspace affiliation hash
+ and if a category match is found, start the app in the specified workspace.
+
+If no match is found, start the app on workspace 5."
+  (interactive)
+  (cl-loop for category in (exwmsetup/get-xdg-desktop-category-list (downcase exwm-class-name))
+         do (setq local-workspace (gethash category exwmsetup/xdg-category-workspace-hash nil))
+         until local-workspace
+         )
+  (exwm-workspace-move-window local-workspace)
+  (notifications-notify :title (format "Opening %s on workspace %d" exwm-class-name local-workspace) :timeout 4000))
 
 ;;; the following functions are based on code included
 ;;; in Systemcrafters' emacs from scratch series "Emacs Desktop Environment"
@@ -77,23 +114,6 @@ The key is the exwm-class-name and the value is hash table of xdg data."
   "Run a daemon program in the background."
   (let ((command-parts (split-string command "[ ]+")))
     (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts))))
-)
-
-(defun exwmsetup/configure-window-by-class ()
-  "When an x windows program is started, compare it to class name
-and configure custom behavior if a match is found."
-  (interactive)
-  (message "Found window: '%s'" exwm-class-name)
-  (pcase exwm-class-name
-    ("Firefox" (exwm-workspace-move-window 2))
-    ("Chromium" (exwm-workspace-move-window 2))
-    ("Thunderbird" (exwm-workspace-move-window 2))
-    ("libreoffice-calc" (exwm-workspace-move-window 4))
-    ("libreoffice-writer" (exwm-workspace-move-window 4))
-    ("libreoffice-startcenter" (exwm-workspace-move-window 4))
-    ("libreoffice-draw" (exwm-workspace-move-window 4))
-    ("libreoffice-impress" (exwm-workspace-move-window 4))
-  )
 )
 
 (defun exwmsetup/set-wallpaper ()
@@ -131,7 +151,10 @@ and configure custom behavior if a match is found."
 
   ;; go back to original workspace
   (exwm-workspace-switch-create 0)
+  (exwmsetup/setup-category-workspace-affiliation)
   (exwmsetup/exwm-xdg-app-mapper)
+  ;;(hash-table-count exwmsetup/exwm-xdg-program-categories-hash)
+  ;;(exwmsetup/get-xdg-desktop-category-list "firefox")
   (message "exwmsetup/exwm-init-hook finished")
 )
 
@@ -149,7 +172,7 @@ and configure custom behavior if a match is found."
   ;; when the window title updates, reset the buffer name
   (add-hook 'exwm-update-title-hook #'exwmsetup/exwm-update-x-windows-buffer)
   ;; manage windows as they are created
-  (add-hook 'exwm-manage-finish-hook #'exwmsetup/configure-window-by-class)
+  (add-hook 'exwm-manage-finish-hook #'exwmsetup/configure-window-by-app-category)
   ;; other exwm startup configuration
   (add-hook 'exwm-init-hook #'exwmsetup/exwm-init-hook)
   (setq exwm-systemtray-height 32)
@@ -167,8 +190,8 @@ and configure custom behavior if a match is found."
         ;;; super key is not available in VM's though... :(
         `(
           ([?\s-r] . exwm-reset)
-          ([s-left] . exwmsetup/goto-previous-frame)
-          ([s-right] . exwmsetup/goto-next-frame)
+          ([s-left] . exwmsetup/goto-previous-workspace)
+          ([s-right] . exwmsetup/goto-next-workspace)
           ([?\s-p] . (lambda() (interactive) (exwmsetup/dunstctl "history-pop")))
           ([?\s-d] . (lambda() (interactive) (exwmsetup/dunstctl "close-all")))
           ([s-up] . windmove-up)
@@ -188,12 +211,12 @@ and configure custom behavior if a match is found."
   ;; We can rotate these workspaces between our displays, which we
   ;; want to default to showing workspace 0-2 at startup, since we have
   ;; 3 displays. The center display is often shared via a KVM switch with another machine.
-  ;; Workspace 0 (left): org-mode windows. Mail inside emacs.
-  ;; Workspace 1 (center): emacs programming: lisp, python, C, markdown, etc.
-  ;; Workspace 2 (right): Browsers, Thunderbird mail reader, IM chat, and similar X programs.
-  ;; Workspace 3 (not shown): Simple games, blender, krita, inkscape
-  ;; Workspace 4 (not shown): Office Productivity Junk
-  ;; Workspace 5 (not shown): Private
+  ;; Workspace 0 (left): org-mode windows. Mail inside emacs, eventually. emacs buffers
+  ;; Workspace 1 (center): emacs programming buffers, xdg category Development
+  ;; Workspace 2 (right): Browsers, Thunderbird mail reader, IM chat xdg categories Network
+  ;; Workspace 3 (not shown): xdg categories Audio, Video, AudioVideo, Graphics, Game
+  ;; Workspace 4 (not shown): xdg categories Office, Education, Science
+  ;; Workspace 5 (not shown): Private, xdg categories System, Settings, Utility
   (setq exwm-workspace-number 6)
   (message "enabling randr")
   (exwm-randr-enable)
